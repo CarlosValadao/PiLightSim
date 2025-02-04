2c68838ce254c42e0262511855b9fe2a8189a031
Carlos Valadão <61719667+CarlosValadao@users.noreply.github.com>
2025-02-03 22:12:08 -0300

    Integração final

--- PiLightSim.c
+++ PiLightSim.c
@@ -1,17 +1,21 @@
 #include "pico/stdlib.h"
 #include "inc/rgb.h"
 
-#define RED    0
-#define YELLOW 1
-#define GREEN  2
-#define TRAFFIC_LIGHT_LED_INTENSITY 40
+#define TRAFFIC_LIGHT_LED_INTENSITY 1
+
+static volatile uint8_t traffic_light_color = RED;
 
 static void set_traffic_light_color(uint8_t color, const rgb_t *pins);
+bool repeating_timer_callback(struct repeating_timer *timer);
+
 
 int main()
 {
     rgb_t pins = {.red=13, .green=11, .blue=12};
     rgb_init(&pins);
+
+    struct repeating_timer timer;    
+    add_repeating_timer_ms(3000, repeating_timer_callback, &pins, &timer);
     while(true);
 }
 
@@ -33,4 +37,12 @@ static void set_traffic_light_color(uint8_t color, const rgb_t *pins)
             rgb_turn_on_green(pins, TRAFFIC_LIGHT_LED_INTENSITY);
             break;
     } // There is no possible default value
+}
+
+bool repeating_timer_callback(struct repeating_timer *timer)
+{
+    rgb_t *rgb_pins = (rgb_t*) timer->user_data;
+    set_traffic_light_color(traffic_light_color, rgb_pins);
+    traffic_light_color = (traffic_light_color + 1) % 3;
+    return true;
 }
